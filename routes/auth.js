af707f26362ad6d36756a023b473b4ba5c840245
StefanCondorache <stefan.condorache@gmail.com>
2024-11-18 22:16:55 +0200

    prima incercare huiovaia

---
+++ routes/auth.js
@@ -0,0 +1,33 @@
+const express = require("express");  // pentru a crea servere web și a gestiona rutele API.
+const router = express.Router();     // submodul ce ajută la organizarea și modularizarea rutelor aplicației
+const dotenv = require('dotenv');    // pentru  încărcarea variabilelor de mediu dintr-un fișier .env în aplicație
+const jwt = require('jsonwebtoken'); // pentru crearea și validarea tokenurilor JWT
+const sha256 = require('sha256');    // pentru criptarea și compararea parolelor
+const DB_URL = process.env.DB_URL;
+
+dotenv.config();
+
+router.post('/autentificare-admin', async (req, res) => {
+    const {username, parola} = req.body;
+
+    try{
+
+        if( username === process.env.ADMIN_USERNAME && sha256(parola) === process.env.ADMIN_PASSWORD) {// ADMIN_PASSWORD ar trebui sa fie incriptata 
+            const token = jwt.sign({ username }, "Secret_Key");
+    
+            return res.status(200).json({token});
+        }
+        else{
+            return res.status(400).json({ message : "Username sau parola incorecta"})
+        }
+    }
+    catch(error){
+        console.error(error);
+        
+    }
+
+    
+
+});
+
+module.exports = router;
