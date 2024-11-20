2875a62ae39527a53b35ce061dda2813c4e7c339
whos-gabi <56652297+whos-gabi@users.noreply.github.com>
2024-11-20 02:56:39 +0200

    proper validation

--- functions.js
+++ functions.js
@@ -1,2 +1,47 @@
-//functii de prelucrare/validare a datelor
-//JWT...
+// functions.js
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const JWT_KEY = process.env.JWT_KEY;
+
+function generateToken(payload) {
+  return jwt.sign(payload, JWT_KEY, { expiresIn: '5h' }); 
+}
+
+function attachUser(req, res, next) {
+    const token = req.cookies.token || null;
+    if (token) {
+      jwt.verify(token, JWT_KEY, (err, user) => {
+        if (!err) {
+          req.user = user;
+        }
+      });
+    }
+    next();
+  }
+
+// functions.js
+function validateToken(req, res, next) {
+    const token = req.cookies.token || null;
+  
+    if (token) {
+      jwt.verify(token, JWT_KEY, (err, user) => {
+        if (err) {
+          // Token is invalid or expired
+          return res.redirect('/auth/login');
+        }
+        req.user = user;
+        next();
+      });
+    } else {
+      // No token provided
+      res.redirect('/auth/login');
+    }
+  }
+  
+
+module.exports = {
+  generateToken,
+  validateToken,
+    attachUser,
+};
